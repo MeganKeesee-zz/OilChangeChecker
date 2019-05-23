2e0eb38ddbd47001de171bddc0cccee4eca2c578
Megan Keesee <hello@megankeesee.com>
2019-05-23 00:00:31 -0700

    add some CSS styling

--- src/recommendation.jsx
+++ src/recommendation.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Recommendation = (props) => {
   if (props.currentOdometerReading - props.prevOdometerReading < props.suggestedInterval) {
     return(
-      <div className="negResp">Nah, you're good  💁🏻‍ You'll need an oil change in {props.suggestedInterval - (props.currentOdometerReading - props.prevOdometerReading)} miles</div>
+      <div className="resp">Nah, you're good  💁🏻‍ You'll need an oil change in {props.suggestedInterval - (props.currentOdometerReading - props.prevOdometerReading)} miles</div>
     )
   } else {
     return (
-      <div>Yep, it's time for an oil change 👩🏻‍🔧 You're {(props.currentOdometerReading - props.prevOdometerReading) - props.suggestedInterval} over the recommended mileage</div>
+      <div className="resp">Yep, it's time for an oil change 👩🏻‍🔧 You're {(props.currentOdometerReading - props.prevOdometerReading) - props.suggestedInterval} over the recommended mileage</div>
     )
   }
 }
